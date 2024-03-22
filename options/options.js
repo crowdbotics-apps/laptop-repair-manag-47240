70ca103f62a1398e3ccea25f1a06623fd39d7fa9
Crowdbotics <team@crowdbotics.com>
2024-03-22 15:11:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "laptop-repair-manag-47240",
+  url: "https://laptop-repair-manag-47240.botics.co",
+  api: "https://laptop-repair-manag-47240.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
